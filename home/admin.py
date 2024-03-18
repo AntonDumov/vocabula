03e5eed700606f6d463862d5982221250e610a29
AntonDumov <anton.dumov@gmail.com>
2024-03-18 16:29:28 +0200

    Added Deck and FlashCard models to home app and registered in admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Deck, FlashCard
 
-# Register your models here.
+
+@admin.register(Deck)
+class DeckAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at', 'updated_at')
+    search_fields = ('name', 'description', 'created_at', 'updated_at')
+
+@admin.register(FlashCard)
+class FlashCardAdmin(admin.ModelAdmin):
+    list_display = ('deck', 'question', 'answer')
+    search_fields = ('deck', 'question', 'answer')
--- home/models.py
+++ home/models.py
@@ -1,3 +1,25 @@
+from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Deck(models.Model):
+    name = models.CharField(max_length=100, verbose_name=_("Deck Name"))
+    description = models.TextField(verbose_name=_("Deck Description"))
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name='decks')
+
+    class Meta:
+        verbose_name = _("Deck")
+        verbose_name_plural = _("Decks")
+
+
+class FlashCard(models.Model):
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE, related_name='flashcards')
+    question = models.TextField(verbose_name=_("FlashCard Question"))
+    answer = models.TextField(verbose_name=_("FlashCard Answer"))
+
+    class Meta:
+        verbose_name = _("FlashCard")
+        verbose_name_plural = _("FlashCards")
